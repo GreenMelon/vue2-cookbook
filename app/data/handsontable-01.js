d1b055d8ba415a6d515ae7454c7e4e98bde28cc0
caigua <caigua@huanleguang.com>
2017-12-21 11:32:16 +0800

    Plugins - vue-handsontable htmlRenderer

--- app/data/handsontable-01.js
+++ app/data/handsontable-01.js
@@ -1,6 +1,27 @@
 export default [
-  ["", "Tesla", "Volvo", "Toyota", "Honda"],
-  ["2017", 10, 11, 12, 13],
-  ["2018", 20, 11, 14, 13],
-  ["2019", 30, 15, 12, 13]
+    {
+        id: 1,
+        name: 'A',
+        date: '1996-01-01',
+        active: false,
+        checked: false,
+        type: 'checkbox',
+        text: '紧身',
+    },{
+        id: 2,
+        name: 'B',
+        date: '2010-01-01',
+        active: true,
+        checked: false,
+        type: 'checkbox',
+        text: '修身',
+    },{
+        id: 3,
+        name: 'C',
+        date: '2016-01-01',
+        active: false,
+        checked: true,
+        type: 'checkbox',
+        text: '宽松',
+    }
 ];
--- app/pages/plugins/vue-handsontable/01.vue
+++ app/pages/plugins/vue-handsontable/01.vue
@@ -4,29 +4,64 @@
 <template>
     <main>
         <div>
-            <VueHotTable
+            <v-hot-table
                 :root="config.root"
                 :settings="config.settings"
-            ></VueHotTable>
+            ></v-hot-table>
         </div>
     </main>
 </template>
 
 <script>
+    // import Handsontable from 'handsontable';
     import VueHotTable from 'vue-handsontable-official';
     import data from '@/data/handsontable-01';
+    // const Handsontable = require('handsontable');
+
+    const RadioRenderer = (instance, td, row, col, prop, value, cellProperties) => {
+        // Handsontable.renderers.TextRenderer.apply(this, arguments);
+    };
+
+    const _data = data.map((item, index) => {
+        return Object.assign({}, item, {
+            content: `<input type="checkbox"> ${item.text}`,
+        });
+    });
 
     export default {
         components: {
-            VueHotTable,
+            'v-hot-table': VueHotTable,
         },
         data() {
             return {
                 config: {
                     root: 'example',
                     settings: {
-                        data: data,
+                        data: _data,
+                        columns: [
+                            {
+                                data: 'id',
+                                type: 'text',
+                            },{
+                                data: 'name',
+                                type: 'text',
+                            },{
+                                data: "date",
+                                type: 'date',
+                                dateFormat: 'YYYY-MM-DD',
+                            },{
+                                data: 'active',
+                                renderer: RadioRenderer,
+                            },{
+                                data: 'checked',
+                                type: 'checkbox',
+                            },{
+                                data: 'content',
+                                renderer: 'html',
+                            }
+                        ],
                         colHeaders: true,
+                        rowHeaders: true,
                     },
                 },
             }
