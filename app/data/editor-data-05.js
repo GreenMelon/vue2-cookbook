1e83b558158e3a43313a4dc357c501daef5171e8
caigua <caigua@huanleguang.com>
2019-01-08 10:54:17 +0800

    Plugins: 普通图框与蒙版图框

---
+++ app/data/editor-data-05.js
@@ -0,0 +1,106 @@
+const TEMPLATE = {
+    "version": "5.6.2-beta.2",
+    "type": "poster",
+    "global": {
+        "zoom": 0.8688888888888888
+    },
+    "layouts": [
+        {
+            "title": "首页海报",
+            "backgroundSize": null,
+            "backgroundImage": null,
+            "backgroundColor": "rgba(255, 255, 255, 0)",
+            "backgroundRepeat": "no-repeat",
+            "height": 820.0100021362305,
+            "width": 750,
+            "className": null,
+            "repeatId": null,
+            "repeatGroup": null,
+            "alias": "首页海报",
+            "elements": [
+                {
+                    "type": "mask",
+                    "category": "",
+                    "opacity": 1,
+                    "padding": [
+                        0,
+                        0,
+                        0,
+                        0
+                    ],
+                    "width": 633,
+                    "height": 632,
+                    "left": 59,
+                    "top": 123,
+                    "transform": {
+                        "a": 1,
+                        "b": 0,
+                        "c": 0,
+                        "d": 1,
+                        "tx": 0,
+                        "ty": 0
+                    },
+                    "boxShadow": {
+                        "color": null
+                    },
+                    "resize": 1,
+                    "dragable": true,
+                    "rotatable": true,
+                    "editable": true,
+                    "frozen": false,
+                    "hidden": false,
+                    "lock": false,
+                    "borderRadius": 0,
+                    "filter": {
+                        "hueRotate": 0,
+                        "saturate": 0,
+                        "brightness": 0,
+                        "gaussianBlur": 0
+                    },
+                    "image": null,
+                    "clip": null,
+                    "url": "//cjxq.oss.aliyuncs.com/cjxq/1/20181225/1_design_upload_1545732463_IYk8RR_20181125-180743366-920-1.png",
+                    "mask": "//cjxq.oss.aliyuncs.com/cjxq/1/20181225/1_design_upload_1545732463_IYk8RR_20181125-180743366-920-1.png",
+                    "imageUrl": "//cjxq.oss.aliyuncs.com/cjxq/1/20181225/1_design_upload_1545732481_uxXKcE_20181125-180801399-6620-1.png",
+                    "imageTransform": {
+                        "a": 1,
+                        "b": 0,
+                        "c": 0,
+                        "d": 1,
+                        "tx": 0,
+                        "ty": 0
+                    },
+                    "imageWidth": 633,
+                    "imageHeight": 632,
+                    "originWidth": 633,
+                    "originHeight": 632,
+                    "effectedImage": null,
+                    "effectedImageWidth": 0,
+                    "effectedImageHeight": 0,
+                    "effectedImageOffsetLeft": 0,
+                    "effectedImageOffsetTop": 0,
+                    "imageEffects": [],
+                    "imageEffectsHash": "",
+                    "aggregatedColors": [],
+                    "mainColor": null,
+                    "effectScale": 1,
+                    "version": "5.6.5-beta.6",
+                    "effectedHeight": 0,
+                    "effectedWidth": 0,
+                    "effectedLeft": 0,
+                    "effectedTop": 0
+                }
+            ],
+            "top": 0,
+            "customData": {
+                "presettingtype": "poster",
+                "presettingname": "首页海报",
+                "loopType": "loop"
+            },
+            "moduleId": 1,
+            "tid": 79813
+        }
+    ]
+};
+
+export default TEMPLATE;
