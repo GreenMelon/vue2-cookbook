00c86b60bd150cd06842bd894eb8b9bdaadc5d9a
caigua <caigua@huanleguang.com>
2019-03-06 15:06:13 +0800

    Plugins: 识别背景色

---
+++ app/pages/plugins/psd-to-templet/04.vue
@@ -0,0 +1,90 @@
+<style lang="less" scoped>
+    .editor-wrapper {
+        position: relative;
+    }
+</style>
+
+<template>
+    <main>
+        <div>
+            <h1>测试 PSD 的背景色</h1>
+            <input
+                @change="parsePSD"
+                accept=".psd"
+                type="file"
+            >
+        </div>
+        
+        <div class="editor-wrapper">
+            <editor
+                ref="editor"
+                :editor-options="editorOptions">
+            </editor>
+        </div>
+    </main>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import VuePosterEditor from 'vue-poster-editor';
+    import PsdToTemplet from '@gaoding/psd-to-templet';
+
+    Vue.use(VuePosterEditor);
+
+    export default {
+        data() {
+            return {
+                zoom: 0.5,
+                editorOptions: {
+                    fontList: [],
+                    mode: 'flow',
+                },
+            }
+        },
+        computed: {
+            editor() {
+                return this.$refs.editor;
+            },
+        },
+        methods: {
+            isBackgroundElement(element) {
+                return element.category && element.category.indexOf('bg_') !== -1;
+            },
+            parsePSD(ev) {
+                const { editor, isBackgroundElement } = this;
+                const { files } = ev.srcElement;
+
+                const options = {
+                    parseSVG: true,
+                    groupMode: 'tag', // flat tag merge
+                    defaultTextType: 'block',
+                    imageQuality: 20,
+                    imageMaxWidth: 5000,
+                    imageMaxHeight: 5000,
+                    onProgress: (data) => {
+                        console.log(data);
+                    },
+                };
+
+                return PsdToTemplet(files[0], options)
+                    .then(layouts => {
+                        console.log(layouts);
+
+                        return editor.setTemplet({
+                            layouts,
+                        });
+                    })
+                    .catch(err => console.error)
+            },
+            initEditor() {
+                this.editor.$events.$on('editor.templet.ready', () => {
+                    this.editor.zoom = this.zoom;
+                    window.editor = this.editor;
+                });
+            },
+        },
+        mounted() {
+            this.initEditor();
+        },
+    };
+</script>
