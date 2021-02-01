931dfb95165215d01f87e3cfe758351b26a56113
caigua <caigua@huanleguang.com>
2021-02-01 17:33:21 +0800

    :sparkles: brace editor

--- app/pages/plugins/brace/01.vue
+++ app/pages/plugins/brace/01.vue
@@ -1,68 +1,56 @@
-<style lang="less" scoped>
-    #brace {
-        height: 300px;
-    }
-</style>
-
 <template>
     <main>
-        <button @click="insert" type="button">insert</button>
-        <button @click="validate" type="button">validate</button>
+        <button @click="importData">导入</button>
+        <button @click="exportData">导出</button>
+
         <div id="brace"></div>
-        <p v-text="isValid"></p>
     </main>
 </template>
 
 <script>
-// import brace from 'brace';
-// import 'brace/mode/javascript';
-// import 'brace/theme/monokai';
+import * as ace from 'brace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 export default {
     data() {
         return {
             editor: null,
-
-            code: {
+            users: [{
                 name: 'caigua',
-                age: 24,
-                favor: ['nba', 'writting']
-            },
-
-            isValid: false
+                sex: 1,
+                favor: ['nba'],
+            }],
         }
     },
-    methods: {
-        insert() {
-            let value = JSON.stringify(this.code);
-            this.editor.setValue(JSON.stringify(JSON.parse(value), null, '\t'));
 
-            // this.editor.setValue(JSON.stringify(this.code), null, '\t');
-        },
-        validate() {
-            // const isJSON = json => {
-            //     var isJson = typeof json == 'object' && Object.prototype.toString.call(json).toLowerCase() == '[object object]' && !json.length;
-            //     return isJson;
-            // };
-
-            const isJSON = json => {
-                try {
-                    JSON.parse(json);
-                    return true;
-                } catch(e) {
-                    console.log(e);
-                    return false;
-                }
-            }
-
-            let code = this.editor.getValue();
-            this.isValid = isJSON(code);
-        }
-    },
     mounted() {
-        // let editor = this.editor = ace.edit('brace');
-        // editor.getSession().setMode('ace/mode/javascript');
-        // editor.setTheme('ace/theme/monokai');
-    }
+        this.init();
+    },
+
+    methods: {
+        init() {
+            this.editor = ace.edit('brace');
+            this.editor.getSession().setMode('ace/mode/javascript');
+            this.editor.setTheme('ace/theme/monokai');
+        },
+
+        importData() {
+            const value = JSON.stringify(this.users);
+            this.editor.setValue(JSON.stringify(JSON.parse(value), null, '\t'));
+        },
+
+        exportData() {
+            const data = this.editor.getValue();
+            console.log(data);
+        }
+    },
 };
 </script>
+
+<style lang="less" scoped>
+#brace {
+    margin-top: 10px;
+    height: 300px;
+}
+</style>
